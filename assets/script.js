385e0de8350f85267eb0b46c6d19dd7266f6289f
landonjett <landonjett@gmail.com>
2023-12-09 18:34:35 -0600

    added basic styling, background photo, and api key and varialbles to js

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Constants 
+
+const apiKey = 'bc66b79e14624947da621b41ff464f47';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/';
+
+// DOM Elements 
+
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const currentWeatherDiv = document.getElementById('current-weather');
+const forecastDiv = document.getElementById('forecast');
+const searchHistoryDiv = document.getElementById('search-history');
